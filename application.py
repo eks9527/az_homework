f70cbb1cc619989de45abb426c0ffec98b23c07d
eks9527 <eks9527@gmail.com>
2021-01-17 15:30:53 +0800

    linebot

--- application.py
+++ application.py
@@ -24,12 +24,30 @@ from imgur_python import Imgur
 from PIL import Image, ImageDraw, ImageFont
 import time
 
-app = Flask(__name__)
 
+LINE_SECRET = "339c696e22382f31a3a90bd8779034fe"
+LINE_TOKEN = "kxKojuWYAbWG8CaGxCB/bQVj4F/LRQJoAZn0BILkiUdUAk82DtBrU4oTpLmW21iZRwNwXgmf6aQKiJWW06PquDqJhrYlcLGUr9JocLs1sBbaZ+MfotCnuKexBJZNtxhaZM1JHDIlmJXt7wXAvvWc9QdB04t89/1O/w1cDnyilFU="
+LINE_BOT = LineBotApi(LINE_TOKEN)
+HANDLER = WebhookHandler(LINE_SECRET)
+
+app = Flask(__name__)
 
 @app.route("/")
 def hello():
     "hello world"
     return "Hello World!!!!!"
 
+@app.route("/callback", methods=["POST"])
+def callback():
+    # X-Line-Signature: 數位簽章
+    signature = request.headers["X-Line-Signature"]
+    print(signature)
+    body = request.get_data(as_text=True)
+    print(body)
+    try:
+        HANDLER.handle(body, signature)
+    except InvalidSignatureError:
+        print("Check the channel secret/access token.")
+        abort(400)
+    return "OK"
 
